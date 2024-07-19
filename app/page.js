38c861add1782aee333c8de3d78443a8389993b0
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-07-19 17:56:19 +0500

    Updated

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]); 
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <>
+    </>
   );
 }
