b31a81f7eeb711d5bb601f7799aa85b13906e42c
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-23 11:06:20 +0500

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['salmon-tremendous-pike-190.mypinata.cloud'],
@@ -9,7 +8,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://nextjs-signin-and-signup.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
